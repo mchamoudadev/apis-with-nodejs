ce6d53d6457058ded0af34a74677038e4cab659b
mchamoudadev <mchamuuda@gmail.com>
2025-05-31 21:42:12 +0300

    added production url

--- frontend/src/lib/api/apiClient.js
+++ frontend/src/lib/api/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import useAuthStore from "../store/authStore";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://mentorship-api-jys6.onrender.com/api";
 
 const api = axios.create({
     baseURL: API_URL,
